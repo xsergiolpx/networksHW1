9cea0c7f73fa95b0f7d5f2cb7c5fd42f97e83b96
arual498 <laurettagiuliano@hotmail.it>
2017-05-14 17:00:19 +0200

    Update irreducibility.py

--- laura/irreducibility.py
+++ laura/irreducibility.py
@@ -1,4 +1,5 @@
 #from sergio.pergraph import *
+import numpy as np
 def connectivityIrr(A):
     '''
         Check the connectivity of a graph 
@@ -6,10 +7,10 @@ def connectivityIrr(A):
         
         :return: True if The adjacency matrix is irreducible (the graph is connected), False if not
         '''
-    I=0
+    I=np.identity(len(A))
     for i in range(0, len(A)):
        I+=A**i
-    if I>0:
+    if (I>0).all():
         return True
         #print(' The adjacency matrix is irreducible,the graph is connected')
     else:
