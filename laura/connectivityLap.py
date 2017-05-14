af74f69440526e8c8d3ba47dfd8e13f9e06576d6
arual498 <laurettagiuliano@hotmail.it>
2017-05-14 17:10:14 +0200

    Update connectivityLap.py

--- laura/connectivityLap.py
+++ laura/connectivityLap.py
@@ -1,8 +1,8 @@
-from numpy import linalg as LA, sort
+from numpy import linalg as LA
 
 def connectivityLap(A):
     L=(1/2)*A.transpose()*A
-    eigval= sort(LA.eigvalsh(A))
+    eigval= LA.eigvalsh(A)
     if eigval[1]>0:
         return True
     else :
