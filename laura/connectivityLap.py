c32bb5b39f569f6f3ad8ead59cd8f5f2c7d06cd9
arual498 <laurettagiuliano@hotmail.it>
2017-05-15 19:06:37 +0200

    Update connectivityLap.py

--- laura/connectivityLap.py
+++ laura/connectivityLap.py
@@ -1,9 +1,23 @@
 from numpy import linalg as LA
+import time
+import numpy as np
+def Degree(A):
+   D = np.zeros((len(A), len(A)))
+   for i in range(0,len(A)):
+      D[i,i]=sum(A[i,:])
+   return D
 
 def connectivityLap(A):
-    L=(1/2)*A.transpose()*A
-    eigval= LA.eigvalsh(A)
-    if eigval[1]>0:
+    '''
+           Check the connectivity of a graph 
+           :param A: adjacency matrix (nxn)
+
+           :return: True if The graph is connected, False if not
+           '''
+    D=Degree(A)
+    L = D-A
+    eigval = LA.eigvalsh(L)
+    if eigval[1] > 0:
         return True
-    else :
+    else:
         return False
