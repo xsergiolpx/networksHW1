717e0a43d1d78687dc1598c8e90b3b0596d79971
Giusy Rose Tiro <giusy.tiro@gmail.com>
2017-05-17 12:38:38 +0200

    test

--- giusy/point 5.py
+++ giusy/point 5.py
@@ -1,6 +1,7 @@
 from sergio.pergraph import *
 from laura.irreducibility import *
 from sergio.conectivity_method_2 import *
+from rana.RegularGraph import *
 from giusy.bfs import *
 import numpy as np
 import matplotlib.pyplot as plt
@@ -28,6 +29,8 @@ print(l)
 plt.plot(prob, l, 'ro')
 plt.show()
 '''
+###############  5.1   ##################
+'''
 N=100
 prob=np.arange(0,1,0.1)
 l=[]
@@ -46,3 +49,30 @@ for p in prob:
 
 plt.plot(prob, l, 'ro')
 plt.show()
+'''
+###############  5.2   ##################
+
+N=5
+r_values=[2,4,8,16]
+n_values=np.arange(2,100,1)
+l=[]
+for r in r_values:
+    for n in n_values:
+        con = 0
+        for i in range(1,N+1):
+            if n>r and (n*r%2==0):
+                g=regular_graph(n,r)
+                #m = graph_to_matrix(g)
+                #L = matrix_L(g)
+                c = bfs(g,1)
+                if c==True:
+                    con=con+1
+        l.append(con/N)
+        print(r)
+        print(n)
+
+print(l)
+#plt.plot(n_values, l, 'ro')
+#plt.show()
+
+
